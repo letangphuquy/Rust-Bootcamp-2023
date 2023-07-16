5e0233a783bdd26a735ab43964e18cb2056179f7
Le Tang Phu Quy <letangphuquy4@gmail.com>
2023-07-16 11:46:04 +0700

    Finished 'generic-type'. So basic!

--- exercises/generic-type/src/lib.rs
+++ exercises/generic-type/src/lib.rs
@@ -1,5 +1,11 @@
+use std::mem::swap;
+
 // Exercise 1 
 // Implement struct Point to make it work.
+struct Position<T> {
+    x: T,
+    y: T
+}
 // Make it compile
 fn exercise1() {
     let integer = Position { x: 5, y: 10 };
@@ -13,7 +19,7 @@ fn exercise1() {
 // Make it compile
 struct Point<T> {
     x: T,
-    y: T,
+    y: String,
 }
 
 fn exercise2() {
@@ -26,12 +32,12 @@ fn exercise2() {
 // Exercise 3
 // Make it compile
 // Add generic for Val to make the code work, DON'T modify the code in `main`.
-struct Val {
-    val: f64,
+struct Val<T> {
+    val: T,
 }
 
-impl Val {
-    fn value(&self) -> &f64 {
+impl<T> Val<T> {
+    fn value(&self) -> &T {
         &self.val
     }
 }
@@ -49,23 +55,47 @@ fn exercise3() {
 // Implementing logic
 // Run tests
 
-fn find_max<T>(collection: &[T]) -> Option<&T> {
-    todo!()
+fn find_max<T: PartialOrd>(collection: &[T]) -> Option<&T> {
+    if collection.len() == 0 { None }
+    else {
+        let mut largest = &collection[0];
+        for item in collection {
+            if item > largest {
+                largest = item;
+            }
+        }
+        Some(largest)
+    }
 }
 
 // Exercise 5 
 // Reverse the elements in a collection
 // Make it compile 
 // Run tests 
-fn reverse_collection<T>(collection: &[T]) {
-    todo!()
+fn reverse_collection<T: Clone>(collection: &mut [T]) {
+    if collection.is_empty() { return (); }
+    let mut l:usize = 0;
+    let mut r = collection.len()-1;
+    while l < r {
+        let tmp = collection[l].clone();
+        collection[l] = collection[r].clone();
+        collection[r] = tmp;
+        l += 1;
+        r -= 1;
+    }
 }
 
 
 // Exercise 6
 // Function to check if a collection contains a specific value
-fn contains_value<T>(collection: &[T], value: &T) -> bool {
-    todo!()
+fn contains_value<T: PartialEq>(collection: &[T], value: &T) -> bool {
+    if collection.is_empty() { false }
+    else {
+        for item in collection {
+            if item == value { return true; }
+        }
+        false
+    }
 }
 
 // Unit tests
@@ -94,6 +124,7 @@ mod tests {
         assert_eq!(find_max(&empty), None);
     }
 
+    
     // Test for exercise 5
     #[test]
     fn test_reverse_collection_with_numbers() {
@@ -101,7 +132,7 @@ mod tests {
         reverse_collection(&mut numbers);
         assert_eq!(numbers, vec![5, 4, 3, 2, 1]);
     }
-
+    
     // Test for exercise 5
     #[test]
     fn test_reverse_collection_with_strings() {
@@ -109,7 +140,7 @@ mod tests {
         reverse_collection(&mut strings);
         assert_eq!(strings, vec!["durian", "cherry", "banana", "apple"]);
     }
-
+    
     // Test for exercise 5
     #[test]
     fn test_reverse_collection_with_empty_collection() {
@@ -117,7 +148,7 @@ mod tests {
         reverse_collection(&mut empty);
         assert_eq!(empty, Vec::<i32>::new());
     }
-
+    
     // Test for exercise 6
     #[test]
     fn test_contains_value_with_numbers() {
@@ -125,7 +156,7 @@ mod tests {
         assert_eq!(contains_value(&numbers, &3), true);
         assert_eq!(contains_value(&numbers, &6), false);
     }
-
+    
     // Test for exercise 6
     #[test]
     fn test_contains_value_with_strings() {
@@ -133,12 +164,11 @@ mod tests {
         assert_eq!(contains_value(&strings, &"banana"), true);
         assert_eq!(contains_value(&strings, &"grape"), false);
     }
-
+    
     // Test for exercise 6
     #[test]
     fn test_contains_value_with_empty_collection() {
         let empty: Vec<i32> = Vec::new();
         assert_eq!(contains_value(&empty, &5), false);
     }
-
 }
